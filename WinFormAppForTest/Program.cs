7af6ea30e50e8e0cb0f76ff53866c24c5ff68eb6
Sushilov Vitaly <sushilov.vit@gmail.com>
2020-11-13 14:54:43 +0200

    MC3

--- WinFormAppForTest/Program.cs
+++ WinFormAppForTest/Program.cs
@@ -17,7 +17,7 @@ namespace WinFormAppForTest
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            // Master 1
+            // Master 2
         }
     }
 }
